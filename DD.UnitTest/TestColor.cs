583b0f28444a70e84b4c011aeef6424af8d0915f
ueda.a.takashi <Ueda.A.Takashi@gmail.com>
2013-06-03 12:07:12 +0000

    LineReaderの改造

---
+++ DD.UnitTest/TestColor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DD.UnitTest {
+    [TestClass]
+    public class TestColor {
+        [TestMethod]
+        public void Test_New () {
+            var col = new Color (1, 2, 3, 4);
+            Assert.AreEqual (1, col.R);
+            Assert.AreEqual (2, col.G);
+            Assert.AreEqual (3, col.B);
+            Assert.AreEqual (4, col.A);
+        }
+
+        [TestMethod]
+        public void Test_Predefined () {
+            Assert.AreEqual (new Color(255,255,255,255), Color.White);
+            Assert.AreEqual (new Color(0,0,0,255), Color.Black);
+        }
+    }
+}
---
+++ DD.UnitTest/TestTimeCounter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DD.UnitTest {
+    [TestClass]
+    public class TestTimeCounter {
+
+        [TestMethod]
+        public void Test_New_1 () {
+            int interval = 0;
+
+            using (var c = new TimeCounter (5, 1)) {
+                c.Elapsed += () => interval += 1;
+                c.Start ();
+
+                while (c.IsRunning) {
+                    System.Threading.Thread.Sleep (1);
+                }
+                System.Threading.Thread.Sleep (1);
+
+                Assert.AreEqual (5, c.Count);
+                Assert.AreEqual (5, interval);
+            }
+        }
+
+        /// <summary>
+        /// インターバル = 0
+        /// </summary>
+        /// <remarks>
+        /// すべてのイベントは呼ばれずにカウントアップが終わる。
+        /// </remarks>
+        [TestMethod]
+        public void Test_New_3 () {
+            int interval = 0;
+
+            using (var c = new TimeCounter (5, 0)) {
+                c.Elapsed += () => interval += 1;
+              c.Start ();
+
+                while (c.IsRunning) {
+                    System.Threading.Thread.Sleep (1);
+                }
+                System.Threading.Thread.Sleep (1);
+
+                Assert.AreEqual (5, c.Count);
+                Assert.AreEqual (0, interval);
+             }
+        }
+
+
+    }
+}
